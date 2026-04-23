2f6205a3dc045c83bd53348c3dd841aeda1b972d
locke 徐 <1045604318@qq.com>
2026-04-23 14:08:17 +0800

    删除当前未用到的bundle包

--- assets/subpackages/l_travelValentine/Scripts/scripts/E06Letter.ts
+++
@@ -1,6 +0,0 @@
-import ESimpleSpecial from '../../../../Scripts/ESimpleSpecial';
-@mj.class
-export default class E06Letter extends ESimpleSpecial {
-  static prefabUrl = "prefabs/E06Letter";
-  static bundleName = "l_travelValentine";
-}
--- assets/subpackages/l_travelValentine/Scripts/scripts/E06Normal.ts
+++
@@ -1,6 +0,0 @@
-import ESimpleNormal from '../../../../Scripts/ESimpleNormal';
-@mj.class
-export default class E06Normal extends ESimpleNormal {
-  static prefabUrl = "prefabs/E06Normal";
-  static bundleName = "l_travelValentine";
-}
