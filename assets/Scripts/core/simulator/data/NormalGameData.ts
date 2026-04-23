534d4a451420a46e3767933d69f9a0571a344467
locke 徐 <1045604318@qq.com>
2026-04-23 14:07:43 +0800

    自定义进入对应关卡逻辑

--- assets/Scripts/core/simulator/data/NormalGameData.ts
+++ assets/Scripts/core/simulator/data/NormalGameData.ts
@@ -3,11 +3,28 @@ import GameData from './GameData';
 @mj.class("NormalGameData")
 export default class NormalGameData extends GameData {
   _gameType = MjGameType.Normal;
+  /** 主线本地调试：改成目标关卡号 (>0) 后进关会抽该关；须配合下面重写避免走「续关」旧棋盘。上线务必改回 0。 */
+  static DEBUG_FORCE_LEVEL = 0;
   constructor() {
     super();
     this.isLocalEmpty(this.localData.rankCardCount) && (this.localData.rankCardCount = 0);
     this.isLocalEmpty(this.localData.isBreakBest) && (this.localData.isBreakBest = false);
     this.isLocalEmpty(this.localData.preBestScore) && (this.localData.preBestScore = 0);
+  }
+  getLevelId() {
+    return NormalGameData.DEBUG_FORCE_LEVEL || super.getLevelId();
+  }
+  getLevelData() {
+    if (NormalGameData.DEBUG_FORCE_LEVEL > 0 && this.localData.levelId !== NormalGameData.DEBUG_FORCE_LEVEL) {
+      return "";
+    }
+    return super.getLevelData();
+  }
+  getOriginalLevelData() {
+    if (NormalGameData.DEBUG_FORCE_LEVEL > 0 && this.localData.levelId !== NormalGameData.DEBUG_FORCE_LEVEL) {
+      return "";
+    }
+    return super.getOriginalLevelData();
   }
   resetGameData() {
     super.resetGameData.call(this);
