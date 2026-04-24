f3014c7369ed4454d1f752cdc4421a3df73bfa93
locke 徐 <1045604318@qq.com>
2026-04-24 16:59:10 +0800

    删除多语言配置➕逻辑（默认英文）

--- assets/Scripts/framework/data/I18NStrings.ts
+++ assets/Scripts/framework/data/I18NStrings.ts
@@ -1,23 +1,16 @@
 import I18NComponent from '../../component/I18NComponent';
 import { ConfigType } from '../../gamePlay/base/data/ConfigType';
-import LoginModel from '../../gamePlay/login/model/LoginModel';
-import { formatLanguageCodeToString } from '../utils/CommonUtils';
 import { DataReader } from './DataReader';
 export default class I18NStrings {
   static stringsCfg = [];
   static defaultLanguage = "en-US";
   static get(e, t = e) {
     if (!e) return t || "";
-    var o = LoginModel.getInstance().language,
-      n = formatLanguageCodeToString(o);
-    -1 == ConfigType.tableList.indexOf("i18n_config_" + n) && (n = "EN_US");
-    var s = DataReader.getByKey(ConfigType.i18n_config, e, n);
-    return s ? s.Value : t || e;
+    var o = DataReader.getByKey(ConfigType.i18n_config, e, "EN_US");
+    return o ? o.Value : t || e;
   }
   static getCN(e, t = e) {
-    if (!e) return t || "";
-    var o = DataReader.getByKey(ConfigType.i18n_config, e, "ZH_CN");
-    return o ? o.Value : t || e;
+    return this.get(e, t);
   }
   static setText(e, t, o = "", i = []) {
     if (!cc.isValid(e)) return false;
